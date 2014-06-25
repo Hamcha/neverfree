2f640e7c22157b64e166e22dbb6ec78f281d5f5a
Hamcha <zikyky@gmail.com>
2014-06-25 14:52:03 +0200

    Better pony assets

--- Assets/Scripts/Gameplay/Projectile.cs
+++ Assets/Scripts/Gameplay/Projectile.cs
@@ -10,16 +10,20 @@ public class Projectile : MonoBehaviour
 
     public  Vector3 initialVelocity = Vector3.zero;
     private Vector3 velocity;
+    private SpriteRenderer sprite;
 
     void Start()
     {
-        //velocity  = Quaternion.AngleAxis(angle, Vector3.forward) * initialVelocity;
         velocity += speed * Vector3.right;
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
         velocity -= velocity.normalized * deceleration * Time.deltaTime;
+        Color color = sprite.color;
+        color.a = velocity.magnitude;
+        sprite.color = color;
         if (velocity.magnitude < threshold) Destroy(gameObject);
         transform.Translate(velocity * Time.deltaTime);
     }
