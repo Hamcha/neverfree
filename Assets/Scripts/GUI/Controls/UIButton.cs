2de71b0f4d8d9ec26b1f3e872eabd26e116e9244
Hamcha <zikyky@gmail.com>
2014-06-30 19:10:19 +0200

    Starting work on UI library

---
+++ Assets/Scripts/GUI/Controls/UIButton.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIButton : MonoBehaviour {
+    public delegate void ClickedHandler(UIButton instance);
+    public event ClickedHandler Clicked;
+    public event ClickedHandler OnFocus, OnBlur;
+
+    public bool enabled = true;
+    public bool hold = false;
+    public ButtonStatus status = ButtonStatus.Idle;
+
+    public enum ButtonStatus {
+        Idle,
+        Focused,
+        Pressed
+    };
+
+    void OnMouseDown() {
+        if (!enabled) return;
+
+        if (status != ButtonStatus.Pressed) {
+            status = ButtonStatus.Pressed;
+            if (Clicked != null) Clicked(this);
+        } else if (hold) {
+            status = ButtonStatus.Idle;
+            if (Clicked != null) Clicked(this);
+        }
+    }
+
+    void OnMouseUp() {
+        if (!enabled) return;
+
+        if (status == ButtonStatus.Pressed && !hold) {
+            status = ButtonStatus.Idle;
+        }
+    }
+
+    void OnMouseEnter() {
+        if (OnFocus != null) OnFocus(this);
+    }
+
+    void OnMouseLeave() {
+        if (OnBlur != null) OnBlur(this);
+    }
+}
---
+++ Assets/Scripts/GUI/Controls/UIButtonSprite.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIButtonSprite : MonoBehaviour {
+
+    public Sprite idle, focus, active, hold;
+    private UIButton button;
+    private SpriteRenderer render;
+
+	void Awake() {
+        button = GetComponent<UIButton>();
+        button.OnFocus += (_) => {
+            if (focus != null) render.sprite = focus;
+        };
+        button.OnBlur += (_) => {
+            render.sprite = idle;
+        };
+        button.Clicked += (_) => {
+            if (button.hold && hold != null) {
+                render.sprite = button.status == UIButton.ButtonStatus.Pressed ? hold : idle;
+            } else {
+                if (active != null) render.sprite = active;
+            }
+        };
+        render.sprite = idle;
+	}
+}
