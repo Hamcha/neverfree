6b6faf51e2419cb4031c36b3c72dc5d2e46abd5e
Hamcha <zikyky@gmail.com>
2014-07-02 03:38:20 +0200

    UIDraggable is done

--- Assets/Scripts/GUI/Controls/UIDraggable.cs
+++ Assets/Scripts/GUI/Controls/UIDraggable.cs
@@ -5,4 +5,29 @@ public class UIDraggable : MonoBehaviour {
     public event DragHandler DragStarted, DragFinished;
 
     public bool isDragging { get; private set; }
+
+    private Vector3 mouseOffset;
+
+    void OnMouseDrag() {
+        if (!isDragging) {
+            mouseOffset = GetMousePos() - transform.position;
+            isDragging = true;
+            if (DragStarted != null) DragStarted(this);
+        }
+
+        transform.position = GetMousePos() - mouseOffset;
+    }
+
+    void OnMouseUp() {
+        if (isDragging) {
+            isDragging = false;
+            if (DragFinished != null) DragFinished(this);
+        }
+    }
+
+    Vector3 GetMousePos() {
+        return Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
+                                                          Input.mousePosition.y,
+                                                          transform.position.z));
+    }
 }
