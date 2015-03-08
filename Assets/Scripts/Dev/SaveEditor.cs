b6aa0163173c36d3231a0a6cee78adb07e65be08
Hamcha <zikyky@gmail.com>
2015-03-08 02:15:00 +0100

    [SAVE ED] Add interaction for stance selection

---
+++ Assets/Scripts/Dev/SaveEditor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveEditor : MonoBehaviour {
+
+    public GameObject[] stances;
+    private string currentStance;
+
+    public void Awake() {
+        for (int i = 0; i < stances.Length; i++) {
+            stances[i].GetComponent<Image>().color = new Color(1, 1, 1, 0.3f);
+        }
+
+        //todo basic loading of save
+        //todo fill name/hearts/coins
+        //todo highlight enabled stances
+    }
+
+    public void setCurrentStance(string stanceName) {
+        currentStance = stanceName;
+    }
+
+    public void changedStanceStatus(bool enabled) {
+        SetFocus(currentStance, enabled);
+        //todo generate list and put it somewhere
+    }
+
+    private void SetFocus(string name, bool dofocus) {
+        for (int i = 0; i < stances.Length; i++) {
+            if (stances[i].name == name) {
+                stances[i].GetComponent<Image>().color = new Color(1, 1, 1, dofocus ? 1 : 0.3f);
+                return;
+            }
+        }
+    }
+}
