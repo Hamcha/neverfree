829eb5580d6544cf01245304a6bf436f832de48c
Hamcha <zikyky@gmail.com>
2015-03-12 00:33:37 +0100

    DeleteObject cutscene event

---
+++ Assets/Scripts/Map/Cutscenes/DeleteObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+class DeleteObject : CutsceneEvent {
+    public GameObject toDelete;
+
+    public override IEnumerator DoCutscene() {
+        if (toDelete == null) {
+            Debug.LogError("Target object to delete is inexistent");
+        } else {
+            Destroy(toDelete);
+        }
+        yield break;
+    }
+}
