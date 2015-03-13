779d6299dd8d785f165b1d0f4692ce357cb7ab5b
Hamcha <zikyky@gmail.com>
2015-03-14 00:18:31 +0100

    Add properties as layer filters

--- Assets/Scripts/Map/LayerLimit.cs
+++ Assets/Scripts/Map/LayerLimit.cs
@@ -4,10 +4,15 @@ public class LayerLimit : MonoBehaviour {
     public bool limitAbility;
     public bool limitNotAbility;
     public Player.Ability ability;
+    public bool limitProperty;
+    public bool limitNotProperty;
+    public string propertyName;
 
     void Start() {
         if (limitAbility && Player.Instance.data.abilities.Contains(ability)) return;
         if (limitNotAbility && !Player.Instance.data.abilities.Contains(ability)) return;
+        if (limitProperty && !Player.Instance.data.properties.ContainsKey(propertyName)) return;
+        if (limitNotProperty && Player.Instance.data.properties.ContainsKey(propertyName)) return;
         gameObject.SetActive(false);
     }
 }
