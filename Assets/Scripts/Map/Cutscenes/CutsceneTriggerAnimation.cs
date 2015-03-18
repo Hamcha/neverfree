1258620ef31ea50903f2af665eb1665667200a67
Hamcha <zikyky@gmail.com>
2015-03-18 23:44:35 +0100

    BaseShotCinematic prefab and cutscene utils

--- Assets/Scripts/Map/Cutscenes/CutsceneTriggerAnimation.cs
+++ Assets/Scripts/Map/Cutscenes/CutsceneTriggerAnimation.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 class CutsceneTriggerAnimation : CutsceneEvent {
     public GameObject target = null;
     public string animationTrigger = "";
+    public float delay = 0;
 
     public override IEnumerator DoCutscene() {
+        yield return new WaitForSeconds(delay);
         target.GetComponent<Animator>().SetTrigger(animationTrigger);
         yield break;
     }
---
+++ Assets/Scripts/Utils/Cutscenes/ShotObtain/CinematicShotFactory.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CinematicShotFactory : MonoBehaviour {
+	public GameObject template = null;
+    public int count = 5;
+    public float delay = 0;
+    public float interval = 0.5f;
+
+	void Start () {
+		StartCoroutine(SpawnObjects());
+	}
+
+	IEnumerator SpawnObjects() {
+		yield return new WaitForSeconds(delay);
+		for (int i = 0; i < count; i++) {
+			Instantiate(template, transform.position, transform.rotation);
+			yield return new WaitForSeconds(interval);
+		}
+	}
+}
