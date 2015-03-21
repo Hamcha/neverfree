dbef944216fbab5fca1ef6871f47692c7805a238
Hamcha <zikyky@gmail.com>
2015-03-21 11:37:11 +0100

    CinematicShotEffect, work in progress

---
+++ Assets/Scripts/Utils/Cutscenes/ShotObtain/CinematicShotEffect.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CinematicShotEffect : MonoBehaviour {
+
+    public enum ShotEffectType {
+        OddOffset
+    }
+
+    public ShotEffectType effect;
+    public float strength = 1;
+    public float delay = 5;
+    public float duration = 0; // 0 = endless
+
+    float startTime;
+    bool active = false;
+
+    void Start() {
+        StartCoroutine(WaitDelay());
+    }
+
+	void Update () {
+        if (!active) return;
+        if (Time.time - startTime > duration) {
+            Debug.Log("Finished shot effect (" + effect.ToString() + ")");
+            active = false;
+            Restore();
+        }
+
+	}
+
+    IEnumerator WaitDelay() {
+        yield return new WaitForSeconds(delay);
+        Debug.Log("Starting shot effect (" + effect.ToString() + ")");
+        Apply();
+        startTime = Time.time;
+        active = true;
+    }
+
+    void Apply() {
+
+    }
+
+    void Restore() {
+
+    }
+}
--- Assets/Scripts/Utils/Cutscenes/ShotObtain/CinematicShotFactory.cs
+++ Assets/Scripts/Utils/Cutscenes/ShotObtain/CinematicShotFactory.cs
@@ -1,20 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class CinematicShotFactory : MonoBehaviour {
 	public GameObject template = null;
-    public int count = 5;
-    public float delay = 0;
-    public float interval = 0.5f;
+	public int count = 5;
+	public float delay = 0;
+	public float interval = 0.5f;
+	public List<GameObject> shots;
 
-	void Start () {
+	void Start() {
+		shots = new List<GameObject>();
 		StartCoroutine(SpawnObjects());
 	}
 
 	IEnumerator SpawnObjects() {
 		yield return new WaitForSeconds(delay);
 		for (int i = 0; i < count; i++) {
-			Instantiate(template, transform.position, transform.rotation);
+			shots.Add((GameObject)Instantiate(template, transform.position, transform.rotation));
 			yield return new WaitForSeconds(interval);
 		}
 	}
